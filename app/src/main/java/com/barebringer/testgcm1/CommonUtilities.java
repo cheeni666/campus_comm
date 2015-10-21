660f04547110f4fe99700c7409794d94d50c7869
cheeni666 <vshreenivasbharadwaj@gmail.com>
2015-10-21 15:18:49 +0530

    /-->ToDo splash screen<--/

--- app/src/main/java/com/barebringer/testgcm1/CommonUtilities.java
+++ app/src/main/java/com/barebringer/testgcm1/CommonUtilities.java
@@ -8,8 +8,8 @@ import java.util.ArrayList;
 public final class CommonUtilities {
 
     // give your server registration url here
-    static final String SERVER_URL = "http://10.0.0.109/~kousik/campuscomm/register.php";
-    static final String NEW_URL = "http://10.0.0.109/~kousik/campuscomm/message.php";
+    static final String SERVER_URL = "http://beerfactory.pragyan.org/~kousik/campuscomm/register.php";
+    static final String NEW_URL = "http://beerfactory.pragyan.org/~kousik/campuscomm/message.php";
 
     // Google project id
     static final String SENDER_ID = "835229264934";
